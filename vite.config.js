bf432c8a3fdc79363c556458566b5de279b3d3a5
Vic Segen <vic.segen@gmail.com>
2026-06-08 00:18:18 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 
     build:
     {
-        outDir: '../dist', // Output in the dist/ folder
+        outDir: 'dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
