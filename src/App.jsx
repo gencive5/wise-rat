962c39747c7312d30f1dd35cfb1dc6385ad2d7c7
Vic Segen <vic.segen@gmail.com>
2026-06-09 11:27:03 +0200

    test fiX again

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,7 @@ function App() {
 
 // fortune telling
 const [fortune, setFortune] = useState("")
-const [fontReady, setFontReady] = useState(false)
 
-  useEffect(() => {
-    const font = new FontFace('mr-rat', 'url(MISTER-RAT.woff2) format("woff2")')
-    font.load().then(() => {
-      document.fonts.add(font)
-      setFontReady(true)
-    }).catch(err => console.log('Font loading error:', err))
-  }, [])
 
 const fortunes = [
   "yes", "no", "maybe", "idk", "ummmmm", "uhhhhhhh", "perhaps", "why not", "What am I supposed to do", "sure", "Ew", "eh",
